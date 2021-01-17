f5040010e38e31c9c502d67470b699cda9c8e042
roydensynderen <roydensynderen@gmail.com>
2021-01-16 19:38:40 -0600

    add cart page

---
+++ react-ecommerce/src/components/pages/cart-page/cart-page.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../../../context/cart-context';
+import Layout from '../../shared/layout';
+import CartItem from './cart-item';
+import Total from './total';
+import './cart-page.scss';
+
+const CartPage = () => {
+  const { cartItems, itemCount, total } = useContext(CartContext);
+  return (
+    <Layout>
+      <>
+        <h1>Cart</h1>
+        {
+          cartItems.length === 0 
+            ? <div className='empty-cart'>Your cart is empty</div> 
+            : <>
+                <div className='cart-page'>
+                  <div className='cart-item-container'>
+                    {
+                      cartItems.map(item => <CartItem { ...item } key={ item.id } />)
+                    }
+                  </div>
+                  <Total itemCount={itemCount} total={total} />
+                </div>
+              </>
+        }
+      </>
+    </Layout>
+  )
+}
+
+export default CartPage;
