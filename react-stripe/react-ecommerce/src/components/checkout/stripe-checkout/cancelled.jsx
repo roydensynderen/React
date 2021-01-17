b892b44a3da60cb7e0f1d60d9ffda84c1786da8b
roydensynderen <roydensynderen@gmail.com>
2021-01-16 22:21:37 -0600

    add cart page functions
    remove eslintcache file
    
    initalize server for react stripe
    
    add stripe functionality and webhooks in the backend server
    
    add .env in .gitignore
    
    Delete .DS_Store
    
    Delete .DS_Store
    
    Delete .env
    
    Delete .env
    
    add .env in .gitignore

---
+++ react-stripe/react-ecommerce/src/components/checkout/stripe-checkout/cancelled.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import Layout from '../../shared/layout';
+
+const Cancelled = ({ history }) => {
+  return (
+    <Layout>
+      <div className='checkout'>
+        <h1>Payment Failed</h1>
+        <p>Payment was not successful</p>
+        <div>
+          <button className='button is-black nomad-btn submit'
+            onClick={() => history.push('/shop')}>Continue Shopping</button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default withRouter(Cancelled);
--- react-stripe/react-ecommerce/src/components/pages/cart-page/total.jsx
+++ react-stripe/react-ecommerce/src/components/pages/cart-page/total.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-const Total = ({ itemCount, total, history }) => {
+const Total = ({ itemCount, total, history, clearCart }) => {
   return (
     <div className='total-container'>
       <div className='total'>
         <p>Total Items: { itemCount }</p>
-        <p>{`Total: ${ total }`}</p>
+        <p>{`Total: $${ total }`}</p>
       </div>
 
       <div className='checkout'>
         <button className='button is-black' onClick={() => history.push('/checkout') }>CHECKOUT</button>
-        <button className='button is-white' onClick={() => {}}>CLEAR</button>
+        <button className='button is-white' onClick={() => clearCart()}>CLEAR</button>
       </div>
     </div>
   )
