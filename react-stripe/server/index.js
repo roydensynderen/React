cffdde17668196c9efbc1f216aff42d7a5e605eb
roydensynderen <roydensynderen@gmail.com>
2021-01-17 15:21:05 -0600

    initalize server for react stripe

---
+++ react-stripe/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config({ path: './env' });
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log('Server listening on port', port));
